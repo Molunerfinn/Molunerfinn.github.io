f68737811562925f8b2661db99162c8e12902bd4
Molunerfinn <marksz@teamsz.xyz>
2018-01-06 11:17:23 +0800

    Updated: themes

--- themes/melody/source/js/fancybox.js
+++ themes/melody/source/js/fancybox.js
@@ -1,8 +1,15 @@
 $(function () {
-  var imgList = $('#content-inner img')
+  var imgList = $('.recent-post-item img')
+  if (imgList.length === 0) {
+    imgList = $('#post-content img')
+  }
   for (var i = 0; i < imgList.length; i++) {
     var $a = $('<a href="' + imgList[i].src + '" data-fancybox="group" data-caption="' + imgList[i].alt + '" class="fancybox"></a>')
-    $(imgList[i]).wrap($a)
+    var alt = imgList[i].alt
+    var $wrap = $(imgList[i]).wrap($a)
+    if (alt) {
+      $wrap.after('<div class="img-alt">' + alt + '</div>')
+    }
   }
 
   $().fancybox({
