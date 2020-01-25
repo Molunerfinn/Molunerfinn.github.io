e29c87ce49636565acef690f2f2785e8f59f7574
Molunerfinn <marksz@teamsz.xyz>
2020-01-25 15:02:47 +0800

    Updated: unregister sw

---
+++ source/service-worker.js
@@ -0,0 +1,9 @@
+"use strict"
+if(window.navigator && navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations()
+  .then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
